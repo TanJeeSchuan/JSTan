6d5beb3469977668a3b11f2bfa699cec929ef5ca
TanJeeSchuan <89920999+TanJeeSchuan@users.noreply.github.com>
2021-09-02 14:26:27 +0800

    v 0.2

--- HOI3 Custom Music Tool.py
+++ HOI3 Custom Music Tool.py
@@ -1,8 +1,12 @@
 from os import listdir
+import re
+import os
 
-print(Input the path of the music files:)
+print('Input the path of the music files:')
 
 path = input()
+
+'C:\\GOG Games\Hearts of Iron III\musicmod'
 
 f = open(path + "\songs.txt","w+")
 
@@ -11,7 +15,12 @@ files = listdir(path)
 mp3 = [s for s in files if s.endswith('.mp3')]
 
 for file in mp3:
+    
+    stripedname = file.replace('Hearts of Iron IV Soundtrack - ','')
+    preparedname = re.sub(r'\s+', '', stripedname)
+    os.rename(path+'\\'+file, path+'\\'+preparedname)
+
     f= open(path + "\songs.txt","a")
-    f.write('song ={ \n name = '+ file +' \n\n chance = { \n      modifier = { \n           factor = 2 \n                 } \n          } \n      }\n \n')
+    f.write('song ={ \n name = '+ preparedname +' \n\n chance = { \n      modifier = { \n           factor = 2 \n                 } \n          } \n      }\n \n')
     f.close()
-    print(file + ' added')
+    print(preparedname + ' added')
